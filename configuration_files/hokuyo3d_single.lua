7f2388a356d193d483d39cfe26324825ba3f0aba
Atsushi Watanabe <atsushi.w@ieee.org>
2019-02-27 22:46:57 +0900

    Add IMU pre-filter

--- configuration_files/hokuyo3d_single.lua
+++ configuration_files/hokuyo3d_single.lua
@@ -47,7 +47,7 @@ options = {
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
-  imu_sampling_ratio = 0.1,
+  imu_sampling_ratio = 0.5,
 }
 
 MAX_3D_LASER_RANGE = 25.
@@ -58,17 +58,17 @@ MAX_3D_LASER_RANGE = 25.
 TRAJECTORY_BUILDER_3D.max_range = MAX_3D_LASER_RANGE
 TRAJECTORY_BUILDER_3D.min_range = 0.5
 TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 64
-TRAJECTORY_BUILDER_3D.voxel_filter_size = 0.05
+TRAJECTORY_BUILDER_3D.voxel_filter_size = 0.1
 
 TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter.max_range = 10.
 TRAJECTORY_BUILDER_3D.low_resolution_adaptive_voxel_filter.max_range = MAX_3D_LASER_RANGE
 
 TRAJECTORY_BUILDER_3D.motion_filter.max_time_seconds = 1.0
-TRAJECTORY_BUILDER_3D.motion_filter.max_distance_meters = 0.05
-TRAJECTORY_BUILDER_3D.motion_filter.max_angle_radians = 0.02
+TRAJECTORY_BUILDER_3D.motion_filter.max_distance_meters = 0.1
+TRAJECTORY_BUILDER_3D.motion_filter.max_angle_radians = 0.04
 
-TRAJECTORY_BUILDER_3D.submaps.num_range_data = 16
-TRAJECTORY_BUILDER_3D.submaps.high_resolution = 0.05
+TRAJECTORY_BUILDER_3D.submaps.num_range_data = 32
+TRAJECTORY_BUILDER_3D.submaps.high_resolution = 0.1
 TRAJECTORY_BUILDER_3D.submaps.low_resolution = 0.35
 TRAJECTORY_BUILDER_3D.submaps.high_resolution_max_range = 10.
 TRAJECTORY_BUILDER_3D.submaps.range_data_inserter.hit_probability = 0.65
@@ -80,21 +80,21 @@ TRAJECTORY_BUILDER_3D.submaps.range_data_inserter.miss_probability = 0.4
 MAP_BUILDER.use_trajectory_builder_3d = true
 MAP_BUILDER.num_background_threads = 3
 
-MAP_BUILDER.pose_graph.optimization_problem.huber_scale = 1.0
-MAP_BUILDER.pose_graph.optimize_every_n_nodes = 2
+MAP_BUILDER.pose_graph.optimization_problem.huber_scale = 0.3
+MAP_BUILDER.pose_graph.optimize_every_n_nodes = 3
 MAP_BUILDER.pose_graph.constraint_builder.sampling_ratio = 0.5
 MAP_BUILDER.pose_graph.global_sampling_ratio = 0.1
 MAP_BUILDER.pose_graph.optimization_problem.ceres_solver_options.max_num_iterations = 12
 
 -- Set min_score according to the histogram from cartographer.
-MAP_BUILDER.pose_graph.constraint_builder.min_score = 0.45
-MAP_BUILDER.pose_graph.constraint_builder.global_localization_min_score = 0.55
+MAP_BUILDER.pose_graph.constraint_builder.min_score = 0.5
+MAP_BUILDER.pose_graph.constraint_builder.global_localization_min_score = 0.6
 
 -- Global constraint settings.
 MAP_BUILDER.pose_graph.constraint_builder.max_constraint_distance= 2.0
 MAP_BUILDER.pose_graph.constraint_builder.fast_correlative_scan_matcher_3d.linear_xy_search_window = 1.0
 MAP_BUILDER.pose_graph.constraint_builder.fast_correlative_scan_matcher_3d.linear_z_search_window = 0.5
-MAP_BUILDER.pose_graph.constraint_builder.fast_correlative_scan_matcher_3d.angular_search_window = math.rad(10.)
+MAP_BUILDER.pose_graph.constraint_builder.fast_correlative_scan_matcher_3d.angular_search_window = math.rad(30.)
 
 MAP_BUILDER.pose_graph.global_constraint_search_after_n_seconds = 5.0
 
---
+++ src/imu_compensate.cpp
@@ -0,0 +1,66 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+
+#include <filter.hpp>
+
+class imu_compensate
+{
+private:
+  ros::NodeHandle nh;
+  ros::Subscriber sub_imu;
+  ros::Publisher pub_imu;
+
+  std::shared_ptr<filter> facc[3];
+  double gyro_zero[3];
+  int cnt;
+
+  void cb_imu(const sensor_msgs::Imu::Ptr& msg)
+  {
+    auto imu = *msg;
+    double acc = sqrt(pow(imu.linear_acceleration.x, 2.0) + pow(imu.linear_acceleration.y, 2.0) + pow(imu.linear_acceleration.z, 2.0));
+
+    cnt++;
+    if (cnt == 500)
+    {
+      ROS_ERROR("IMU zero calibrated");
+    }
+    else if (cnt < 500)
+    {
+      double k = 0.01;
+      gyro_zero[0] = gyro_zero[0] * (1.0 - k) + imu.angular_velocity.x * k;
+      gyro_zero[1] = gyro_zero[1] * (1.0 - k) + imu.angular_velocity.y * k;
+      gyro_zero[2] = gyro_zero[2] * (1.0 - k) + imu.angular_velocity.z * k;
+    }
+    imu.angular_velocity.x -= gyro_zero[0];
+    imu.angular_velocity.y -= gyro_zero[1];
+    imu.angular_velocity.z -= gyro_zero[2];
+    imu.linear_acceleration.x = facc[0]->in(imu.linear_acceleration.x);
+    imu.linear_acceleration.y = facc[1]->in(imu.linear_acceleration.y);
+    imu.linear_acceleration.z = facc[2]->in(imu.linear_acceleration.z);
+    pub_imu.publish(imu);
+  }
+
+public:
+  imu_compensate()
+    : nh("~")
+  {
+    sub_imu = nh.subscribe("imu_raw", 1, &imu_compensate::cb_imu, this);
+    pub_imu = nh.advertise<sensor_msgs::Imu>("imu", 2);
+    cnt = 0;
+    facc[0].reset(new filter(filter::FILTER_LPF, 10.0, 0.0));
+    facc[1].reset(new filter(filter::FILTER_LPF, 10.0, 0.0));
+    facc[2].reset(new filter(filter::FILTER_LPF, 10.0, 0.0));
+    gyro_zero[0] = gyro_zero[1] = gyro_zero[2] = 0;
+  }
+};
+
+int main(int argc, char* argv[])
+{
+  ros::init(argc, argv, "imu_compensate");
+
+  imu_compensate imu;
+
+  ros::spin();
+
+  return 0;
+}
